5863d5e4cb02d17b550421b8693c13cdb1f3c4f2
zhangsheng <332827086@qq.com>
2020-12-31 04:28:38 +0800

    添加忽略

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity() {
+    var list: MutableList<String> = mutableListOf()
+    var text: TextView? = null
+    var recycleView: RecyclerView? = null
+
+    var textList: ArrayList<String> =
+        arrayListOf("fafas", "fdasfas", "fdasfdasf", "fdsfaf", "fadfasdsfeewe");
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        list.add("fdsfa")
+        list.add("fsdafs")
+        list.add("fsafdafsdf")
+        list.sort()
+        text = findViewById(R.id.text)
+        text?.text = list.toString()
+        recycleView = findViewById(R.id.recycleView)
+
+        //使用Recycler
+        val layoutManager = GridLayoutManager(this,2)
+        recycleView?.layoutManager = layoutManager
+        val recycleViewAdapter: RecycleViewAdapter = RecycleViewAdapter(textList)
+        recycleView?.adapter = recycleViewAdapter
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/RecycleViewAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * description:
+ * author:zhangsheng
+ * create time:2020-12-30 13:11
+ */
+class RecycleViewAdapter(private val textList: ArrayList<String>) :
+    RecyclerView.Adapter<RecycleViewAdapter.MyHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.layout, parent, false)
+        return MyHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        holder.title.text = textList[position];
+        holder.title.setOnClickListener { Toast.makeText(holder.title.context,textList[position],Toast.LENGTH_LONG).show() }
+
+    }
+
+    override fun getItemCount(): Int = textList.size ?: 0
+
+
+    class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val title: TextView = itemView.findViewById(R.id.tv_list)
+    }
+}
